8f33917b823938ea8dbd652d35dc60ee2c137d65
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2022-08-08 13:15:58 +0200

    diff, prop und integral for all directions

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,51 @@
 //*****************************************************************************
-// my new 7.7.2022
-//                               c i r c e l
+// 8. Aug. 2022
+//                               c i r c l e 
 //
-//                                                              қuran july 2022
+//                                                               қuran wolfgang
 //*****************************************************************************
 #include <Arduino.h>
 #include "BluetoothSerial.h"
 
-int led = 5;
-int onOff;
-
-// motor:
+// defines:
+#define H               HIGH
+#define L                LOW
 
 #define WHEEL_L            2 
 #define WHEEL_R           A4
 #define WHEEL_L_DIRECTION A5 
 #define WHEEL_R_DIRECTION 15 
-#define H               HIGH
-#define L                LOW
-
-volatile int speed;
-volatile int diff;
-volatile int setL, setR, oldSetL, oldSetR;
-volatile int meterL, meterR;    // zurückgelegte Wegstrecke, wie sie errechnet wird
-volatile int actualL, actualR;  // tatsächliche Wegstrecke gemessen
-volatile int directionL, directionR;        // die Richtung in die gefahren werden soll
-volatile int vL, vR;            // Vergleichswert für die pwm Motorsteuerung
-
-volatile int count;
 
 // Blue Tooth:
 #define LEN 100
 #define CR  13
 #define LF  10
+
+// global variables:
+
+int led = 5; // on board led
+int onOff;   // on board led on / off
+
+
+volatile int speed;
+volatile int diff;
+volatile int setL, setR, oldSetL, oldSetR;
+volatile int amountL, amountR;
+volatile int oAmountL, oAmountR;
+volatile int meterL, meterR;    // zurückgelegte Wegstrecke, wie sie errechnet wird
+volatile int actualL, actualR;  // tatsächliche Wegstrecke, gemessen
+volatile int directionL, directionR, oDirL, oDirR;  // die Richtung in die gefahren werden soll
+volatile int diffL, diffR;    
+volatile int vL, vR;            // Vergleichswert für die pwm Motorsteuerung
+
+volatile int count;
+
 BluetoothSerial SerialBT;
 char text[LEN];
 
 // SPEEDOMETER:
 const uint8_t impulsL = 14;
 const uint8_t impulsR = 27;
-volatile uint8_t impulsRcount;   // wheels movement control
-volatile uint8_t impulsLcount;
 
 //  TIMER INTERRUPT:
 hw_timer_t * timer = NULL;
@@ -49,10 +54,16 @@ volatile uint8_t tenMsec;
 volatile boolean flag; 
 
 // Prototypes
+void driveControl(void);
 void IRAM_ATTR onTimer(void);
 void impuls_R_isr(void);
 void impuls_L_isr(void);
 
+
+
+// RTOS not used in this version:
+
+/*
 // structs and types
 typedef struct message MESSAGE;
 struct message
@@ -61,8 +72,7 @@ struct message
 };
 static MESSAGE m, n;
 
-// RTOS not used in this version:
-/*
+
 // TaskFunctions:
 static void taskFuncA(void * arg)
 {
@@ -91,12 +101,11 @@ static void taskFuncB(void * arg)
 void setup() 
 {
 //  int app_cpu = 0;  for RTOS
-    m.x = 7;     // for RTOS
-    n.x = 100;   // for RTOS
+//    m.x = 7;     // for RTOS
+//    n.x = 100;   // for RTOS
 
     delay(500); // pause für serial setup...
     printf("start!\n- - - - - - - - - - - - - - - - - - - - - - - - - - -  -\n");
-  
 
     pinMode(led, OUTPUT);
     digitalWrite(led, HIGH); // invers!  LOW = Led on!
@@ -109,14 +118,17 @@ void setup()
 
   // BlueTooth:
 
-    SerialBT.begin("LoLin32_220714");
+    SerialBT.begin("LoLin32_220808");
     printf("bluetooth startet\n");
     
     // motor:
     speed = diff = 0;
     oldSetL = oldSetR = setL = setR = 0;
+    amountL = amountR = 0;
+    oAmountL = oAmountR = 0;
     meterL = meterR = 0;    
     actualL = actualR = 0;
+    diffL = diffR = 0;
     vL = vR = 0;
 
     pinMode(WHEEL_L, OUTPUT);
@@ -126,20 +138,16 @@ void setup()
 
     digitalWrite(WHEEL_L, L);   // stop !
     digitalWrite(WHEEL_R, L);  // stop !
-    digitalWrite(WHEEL_L_DIRECTION, L); directionL = +1;
-    digitalWrite(WHEEL_R_DIRECTION, H); directionR = +1;
+    digitalWrite(WHEEL_L_DIRECTION, L); oDirL = directionL = +1;
+    digitalWrite(WHEEL_R_DIRECTION, H); oDirR = directionR = +1;
+
 
   // SPEEDOMETER:
 
     count = 0;
 
-    impulsLcount = impulsRcount = 0;  // can später entfernt werden
-
     pinMode(impulsL, INPUT_PULLUP);
     pinMode(impulsR, INPUT_PULLUP);
-    digitalWrite(impulsL, HIGH);
-    digitalWrite(impulsR, HIGH);
-
 
     attachInterrupt(digitalPinToInterrupt(impulsR), impuls_R_isr, FALLING);
     attachInterrupt(digitalPinToInterrupt(impulsL), impuls_L_isr, FALLING);
@@ -148,7 +156,7 @@ void setup()
 
     timer = timerBegin(0, 80, true);
     timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 10, true); // 10 µsec
+    timerAlarmWrite(timer, 10, true); 
     timerAlarmEnable(timer);
 }
 
@@ -165,6 +173,50 @@ void loop()
     {
        c = SerialBT.read();
        text += c;
+
+       switch(c)
+       {
+        case 'a':  // LEDS on
+        break;
+        case 'b':  // LEDS off
+        break;
+        case 'c':  // STOP
+                speed = 0;
+                diff = 0;
+        break;
+        case 'd':  // UP
+                speed = 60;
+                diff = 0;
+        break;
+        case 'e':  // DOWN
+                speed = -60;
+                diff = 0;
+        break;
+        case 'f':  // LEFT UP
+                speed = 60;
+                diff = -60;
+        break;
+        case 'g':  // RIGHT UP
+                speed = 60;
+                diff = 60;
+        break;
+        case 'h':  // ROTATE LEFT
+                speed = 0;
+                diff = -60;
+        break;
+        case 'i':  // ROTATE RIGHT
+                speed = 0;
+                diff = 60;
+        break;
+
+       }
+
+       driveControl();
+
+       sprintf(report,"get: %c\n", c);
+       printf(report);
+
+
     }
 
     if (text.startsWith("ok"))
@@ -181,74 +233,119 @@ void loop()
         // Richtung bleibt derzeit noch weg.... 
     }
 
-/*
-
-    if (text.startsWith("+"))
-    {
-        val++;
-        sprintf(respond,"val %d", val);
-        SerialBT.println(respond);
-    }
-
-    if (text.startsWith("-"))
-    {
-        val--;
-        sprintf(respond,"val %d", val);
-        SerialBT.println(respond);
-    }
-
-    if (text.startsWith("d"))
-    {
-        sprintf(respond,"direction up");
-        digitalWrite(WHEEL_RIGHT_DIRECTION, LOW);        
-        SerialBT.println(respond);
-    }
-
-    if (text.startsWith("D"))
-    {
-        sprintf(respond,"direction down");
-        digitalWrite(WHEEL_RIGHT_DIRECTION, HIGH);        
-        SerialBT.println(respond);
-    }
-
-*/
+/*    if (text.startsWith("+")) */
 
    // Timer
+
    if (flag == 1)
    {
         onOff = (onOff == 0) ? 1 : 0;
         digitalWrite(led, onOff);
         flag = 0;
 
-        // motor:
+        // motor:  integrativer Anteil
+        
+        meterL += amountL; 
+        meterR += amountR;
 
-        meterL += setL; meterR += setR;
-
-        if (actualL > meterL) actualL = meterL - 40;
-        vL = setL + (setL - oldSetL) +  (meterL - actualL);
+        if (amountL == 0) vL = 0;
+        else
+        {
+           diffL >>= 1;  // differentieller Anteil halbiert sich
+           if (actualL > meterL) actualL = meterL - 40;
+           vL = diffL + amountL + meterL - actualL;
+        }
         if (vL > 255) vL = 255;
-        if (vL < 0)  vL = -1; // sollte 0 sein - zum debuggen besser... 
-        oldSetL = setL;
-
-        if (actualR > meterR) actualR = meterR - 40;
-        vR = setR + (setR - oldSetR) +  (meterR - actualR);
+        if (vL < 0)  vL = 0; // sollte 0 sein - zum debuggen besser... 
+ 
+        if (amountR == 0) vR = 0;
+        else
+        {
+            diffR >>= 1;  // differentieller Anteil halbiert sich
+            if (actualR > meterR) actualR = meterR - 40;
+            vR = diffR + amountR + meterR - actualR;
+        }
         if (vR > 255) vR = 255;
-        if (vR < 0)  vR = -1;
-        oldSetR = setR;
+        if (vR < 0)  vR = 0; // sollte 0 sein - zum debuggen besser... 
 
-
-        sprintf(report, "s %02d d %02d setL %02d setR %02d mL %04d mR %04d aL %04d aR %04d vL %02d vR %02d",
-                         speed, diff, setL, setR, meterL, meterR, actualL, actualR, vL, vR );
-
-        printf("%s\n", report);
-
+// Tests:
+//        sprintf(report, "s %02d d %02d setL %02d setR %02d mL %04d mR %04d aL %04d aR %04d vL %02d vR %02d",
+//                         speed, diff, setL, setR, meterL, meterR, actualL, actualR, vL, vR );
+//        printf("%s\n", report);
+//   --- 
         sprintf(respond, "vL %03d vR %03d mR %d aR %d c %d", vL, vR, meterR, actualR, count);
         count = 0;
-        SerialBT.println(respond);
+        // ---- SerialBT.println(respond);
+        // --- August 2022
 
    }
 
 }
+
+void driveControl(void) // speed, diff
+{
+    // erster Schritt: errechne aus speed und diff die setL und setR
+
+    setL = speed - diff;
+    setR = speed + diff;
+
+    // daraus wird zunächst die Richtung festgelegt und der Betrag gesetzt
+
+    if (setL >=  0) 
+    {
+      amountL = setL; 
+      digitalWrite(WHEEL_L_DIRECTION, L); directionL = +1;
+    }
+    else
+    {
+      amountL = -setL;
+      digitalWrite(WHEEL_L_DIRECTION, H); directionL = -1;
+    }
+
+    if (setR >=  0) 
+    {
+      amountR = setR; 
+      digitalWrite(WHEEL_R_DIRECTION, H); directionR = +1;
+    }
+    else
+    {
+      amountR = -setR;
+      digitalWrite(WHEEL_R_DIRECTION, L); directionL = -1;
+    }
+
+    //  hat sich dabei die Richtung verändert, dann werden die Wegstrecken zurückgesetzt:
+
+    if ((directionL != oDirL) || (setL == 0))
+    {
+      meterL = 0;
+      actualL = 0;
+    }
+
+    if ((directionR != oDirR) || (setR == 0))
+    {
+      meterR = 0;
+      actualR = 0;
+    }
+
+    //  Proportionalanteil:
+
+    vL = amountL;
+    vR = amountR;  
+
+    // differentieller Anteil:
+
+    diffL = 2 * (amountL - oAmountL);
+    diffR = 2 * (amountR - oAmountR);
+
+    vL += diffL;
+    vR = diffR;
+
+    oAmountL = amountL;
+    oAmountR = amountR;
+
+
+}
+
 
 //****************************************************************
 // ISR Speedometer:
@@ -256,28 +353,13 @@ void loop()
 
 void impuls_L_isr(void)
 {
-  impulsLcount += directionL;
-  actualL += directionL;
- 
-
-  //vL = meterL - actualL;
-  //if (vL > 255) vL = 255;
-  //if (vL < 0)  vL = 0;
+  actualL++;
 }
 
 void impuls_R_isr(void)
 {
-  impulsRcount += directionR;
-  actualR += directionR;
-  count++;
-
-  //vR = meterR - actualR;
-  //if (vR > 255) vR = 255;
-  //if (vR < 0)  vR = 0;
-
+  actualR++;
 }
-
-
 
 
 //****************************************************************
@@ -302,8 +384,8 @@ void IRAM_ATTR onTimer(void)
     }
 
 
-    if ((tick & 0xff) > vL) digitalWrite(WHEEL_L, L); else digitalWrite(WHEEL_L, H);
-    if ((tick & 0xff) > vR) digitalWrite(WHEEL_R, L); else digitalWrite(WHEEL_R, H);
+    if (((tick << 2) & 0xff) > vL) digitalWrite(WHEEL_L, L); else digitalWrite(WHEEL_L, H);
+    if (((tick << 2) & 0xff) > vR) digitalWrite(WHEEL_R, L); else digitalWrite(WHEEL_R, H);
 
     
 }
